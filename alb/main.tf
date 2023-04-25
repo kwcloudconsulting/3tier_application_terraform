0e092d4eddf3e71677ff9eeef9dfcba5d393bc6e
Karimarie Wilson <kwcloudconsulting@gmail.com>
2023-04-24 21:54:57 -0500

    alb issues

--- alb/main.tf
+++ alb/main.tf
@@ -1,66 +1,56 @@
-# create application load balancer
-resource "aws_lb" "application_load_balancer" {
-  #name               = var.project_name
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [var.alb_security_group_id]
-  subnets            = [var.public_subnet_az1_id, var.public_subnet_az2_id]
-  enable_deletion_protection = false 
-
-  tags   = {
-    Name = "var.project_name--alb"
-  }
-}
-
-# create target group
+# Create a target group
 resource "aws_lb_target_group" "alb_target_group" {
-  #name        = var.project_name
-  target_type = "ip"
+  name_prefix = "tg-"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
 
   health_check {
-    enabled             = true
-    interval            = 300
-    path                = "/"
-    timeout             = 60
-    matcher             = 200
-    healthy_threshold   = 5
-    unhealthy_threshold = 5
-  }
-
-  lifecycle {
-    create_before_destroy = true
+    interval          = 30
+    path               = "/"
+    port               = "traffic-port"
+    protocol           = "HTTP"
+    timeout            = 5
+    healthy_threshold  = 2
+    unhealthy_threshold = 2
   }
 }
 
-# create a listener on port 80 with redirect action
-resource "aws_lb_listener" "alb_http_listener" {
+# Create an ALB
+resource "aws_lb" "application_load_balancer" {
+  name                = "my-alb"
+  internal            = false
+  load_balancer_type  = "application"
+  security_groups     = [var.alb_security_group_id]
+  subnets             = [var.public_subnet_az1_id, var.public_subnet_az2_id]
+}
+
+# Create a listener on port 80 with a redirect action
+resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type = "redirect"
-
     redirect {
-      port        = 443
       protocol    = "HTTPS"
+      port        = "443"
       status_code = "HTTP_301"
     }
   }
 }
 
-# create a listener on port 443 with forward action
-resource "aws_lb_listener" "alb_https_listener" {
-  load_balancer_arn  = aws_lb.application_load_balancer.id
-  port               = 443
-  protocol           = "HTTPS"
-  #ssl_policy         = "ELBSecurityPolicy-TLS-1-2-2017-01" # or use appropriate SSL policy
-  certificate_arn    = var.certificate_arn
+# Create a listener on port 443 with a forward action
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 443
+  protocol          = "HTTPS"
 
   default_action {
     type             = "forward"
   }
 }
+
+
+
